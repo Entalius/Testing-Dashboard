5db710dc5af9dac8547bbf9f0bc7f91bb6814fc7
Entalius <106567546+Entalius@users.noreply.github.com>
2023-04-06 12:54:40 +0200

    Add files via upload

---
+++ src/components/FilterMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FilterMenu = () => {
+  return (
+    <div>
+      {/* Add your filter menu elements here */}
+      <h2>Filter Menu</h2>
+    </div>
+  );
+};
+
+export default FilterMenu;
